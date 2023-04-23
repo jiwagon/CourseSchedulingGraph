400740c0d5c26528d1a95702a3371fc91b94c12d
jiwagon <jiwagon11@gmail.com>
2023-04-22 22:48:46 -0400

    Successful add course tests

--- src/CourseSchedulerTest.java
+++ src/CourseSchedulerTest.java
@@ -7,6 +7,21 @@ class CourseSchedulerTest {
     @org.junit.jupiter.api.Test
     void addCourse() {
 
+        CourseScheduler scheduler = new CourseScheduler();
+
+        // Test adding a single class
+        scheduler.addCourse("ClassA");
+        assertEquals(1, scheduler.courses.size());
+        assertEquals("ClassA", scheduler.courses.get(0).courseName);
+
+        // Test adding multiple classes
+        scheduler.addCourse("ClassB");
+        scheduler.addCourse("ClassC");
+        assertEquals(3, scheduler.courses.size());
+        assertEquals("ClassB", scheduler.courses.get(1).courseName);
+        assertEquals("ClassC", scheduler.courses.get(2).courseName);
+
+        System.out.println(scheduler);
     }
 
     @org.junit.jupiter.api.Test
