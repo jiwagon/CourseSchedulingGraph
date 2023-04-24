920854afa3cc83db71341a28b75ad27878ab9c6b
jiwagon <jiwagon11@gmail.com>
2023-04-24 18:07:48 -0400

    Final Commit

--- src/CourseScheduler.java
+++ src/CourseScheduler.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 //CourseScheduler = graph;
 public class CourseScheduler {
@@ -8,28 +10,6 @@ public class CourseScheduler {
     public CourseScheduler() {
         this.courses = new ArrayList<>();
     }
-
-//    // Method to add a class as a vertex to the graph
-//    public void addCourse(String courseName) {
-//        this.courses.add(new Vertex(courseName));
-//    }
-//
-//    // Helper method to get the vertex by class name
-//    private Vertex getVertex(String className) {
-//        for (Vertex vertex : graph) {
-//            if (vertex.className.equals(className)) {
-//                return vertex;
-//            }
-//        }
-//        return null;
-//    }
-//
-//    // Method to add a prerequisite between two classes as an edge in the graph
-//    public void addPrerequisite(String fromClass, String toClass, int weight) {
-//        Vertex fromVertex = getVertex(fromClass);
-//        Vertex toVertex = getVertex(toClass);
-//        fromVertex.edges.add(new Edge(toVertex, weight));
-//    }
 
     public int getIndex(String vertex) {
         for (int i = 0; i < this.courses.size(); i++) {
@@ -48,7 +28,7 @@ public class CourseScheduler {
             this.courses.add(toAdd);
         }
         else {
-            System.out.println("Vertex" + vertex + " already exists.");
+            System.out.println("Course " + vertex + " already exists.");
         }
         return toAdd;
     }
@@ -73,23 +53,16 @@ public class CourseScheduler {
             end = this.courses.get(endPos);
         }
 
-//        for (int i = 0; i < start.edges.size(); i++) {
-//            if (start.edges.get(i).eVertex.vName == endVer) {
-//                    System.out.println("End Edge already exists in Graph");
-//                    return;
-//                }
-//            }
-//
-//        for (int i = 0; i < start.edges.size(); i++) {
-//            if (end.edges.get(i).eVertex.vName.equals(startVer)) {
-//                System.out.println("Start Edge already exists in Graph");
-//                return;
-//            }
-        //}
+        for (int i = 0; i < start.edges.size(); i++) {
+            if (start.edges.get(i).preRequisite.courseName == toCourse) {
+                    System.out.println("End Edge already exists in Graph");
+                    return;
+                }
+            }
 
         // Add same edge from end to start
         start.edges.add(new Edge(end, weight));
-        end.edges.add(new Edge(start, weight));
+        // end.edges.add(new Edge(start, weight));-- dont need this for directed graph
         // add vertex to startVertex arraylist of edges if edge with endVertex is not existing
         // add vertex to endVertex arraylist of edges if edge with startVertex is not existing
     }
@@ -104,5 +77,36 @@ public class CourseScheduler {
             sb.append("\n");
         }
         return sb.toString();
+    }
+
+        // Breadth-First Traversal
+        public String BFS(String startName, String endName){
+
+        Queue<Vertex> searchQ = new LinkedList<>();
+        ArrayList<Vertex> visitedVertices = new ArrayList<>();
+        ArrayList<String> paths = new ArrayList<>();
+        StringBuilder stb = new StringBuilder();
+
+        int index = this.getIndex(startName);
+        searchQ.add(this.courses.get(index));
+        visitedVertices.add(this.courses.get(index));
+        paths.add(this.courses.get(index).courseName);
+
+        while(!searchQ.isEmpty()){
+            Vertex current = searchQ.remove();
+            stb.append(current.courseName + " - ");
+            int pos = visitedVertices.indexOf(current);
+            if(current.courseName.equals(endName))
+                return paths.get(pos);
+
+            for(Edge edge: current.edges){
+                if(!visitedVertices.contains(edge.preRequisite)){
+                    searchQ.add(edge.preRequisite);
+                    visitedVertices.add(edge.preRequisite);
+                    paths.add(paths.get(pos) + "--" + edge.preRequisite.courseName);
+                }
+            }
+        }
+        return "";
     }
 }
--- src/CourseSchedulerTest.java
+++ src/CourseSchedulerTest.java
@@ -1,4 +1,4 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -6,7 +6,6 @@ class CourseSchedulerTest {
 
     @org.junit.jupiter.api.Test
     void addCourse() {
-
         CourseScheduler scheduler = new CourseScheduler();
 
         // Test adding a single class
@@ -21,7 +20,8 @@ class CourseSchedulerTest {
         assertEquals("IST 210", scheduler.courses.get(1).courseName);
         assertEquals("IST 216", scheduler.courses.get(2).courseName);
 
-        System.out.println(scheduler);
+        // To inform repeated course is being added.
+        scheduler.addCourse("IST 210");
     }
 
     @org.junit.jupiter.api.Test
@@ -45,6 +45,61 @@ class CourseSchedulerTest {
         assertEquals("IST 412", scheduler.courses.get(0).edges.get(1).preRequisite.courseName);
         assertEquals(2, scheduler.courses.get(0).edges.get(1).weight);
 
+        scheduler.addPrerequisite("IST 311", "IST 412", 1);
+        assertEquals(2, scheduler.courses.get(0).edges.size());
+        assertEquals("IST 412", scheduler.courses.get(0).edges.get(1).preRequisite.courseName);
+        assertEquals(2, scheduler.courses.get(0).edges.get(1).weight);
+
+        System.out.println("\nPrerequisite: Courses Led-to");
         System.out.println(scheduler);
+    }
+
+    @Test
+    void BFS() {
+        CourseScheduler scheduler = new CourseScheduler();
+
+        scheduler.addCourse("IST 242");
+        scheduler.addCourse("IST 311");
+        scheduler.addCourse("IST 412");
+        scheduler.addCourse("IST 310");
+        scheduler.addCourse("IST 316");
+
+        // Test adding a prerequisite between two classes
+        scheduler.addPrerequisite("IST 242", "IST 311", 1);
+        scheduler.addPrerequisite("IST 311", "IST 412", 1);
+        scheduler.addPrerequisite("IST 242", "IST 310", 1);
+        scheduler.addPrerequisite("IST 310", "IST 316", 1);
+
+
+        // Original Path
+        System.out.println("\nThe original paths in given graph:");
+        System.out.println(scheduler.BFS("IST 242", "IST 412"));
+        System.out.println(scheduler.BFS("IST 242", "IST 316"));
+
+        // Shorter Path
+        scheduler.addPrerequisite("IST 242", "IST 412", 1);
+        scheduler.addPrerequisite("IST 242", "IST 316", 1);
+
+        System.out.println("The shorter paths that replace original paths in given graph:");
+        System.out.println(scheduler.BFS("IST 242", "IST 412"));
+        System.out.println(scheduler.BFS("IST 242", "IST 316"));
+
+        scheduler.addCourse("A");
+        scheduler.addCourse("B");
+        scheduler.addCourse("C");
+        scheduler.addCourse("D");
+        scheduler.addCourse("E");
+        scheduler.addCourse("F");
+
+        scheduler.addPrerequisite("A", "B", 1);
+        scheduler.addPrerequisite("B", "C", 1);
+        scheduler.addPrerequisite("C", "D", 1);
+        scheduler.addPrerequisite("B", "E", 1);
+        scheduler.addPrerequisite("D", "F", 1);
+
+        System.out.println("\nThe paths available in given graph:");
+        System.out.println(scheduler.BFS("A","E"));
+        System.out.println(scheduler.BFS("B","F"));
+        System.out.println(scheduler.BFS("A","F"));
     }
 }
